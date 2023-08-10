a39c1f0c2a5c939aac0594a83bc67b4c84d19a0d
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2023-08-10 14:10:10 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <div className="flex h-screen justify-center"> 
@@ -8,7 +10,7 @@ export default function Home() {
         </p>
         <div className="pt-8">
         <button className={`w-fit right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 text-black bg-slate-200 dark:bg-[#212933]`}>
-          Get Started
+          <Link href="/equipment_options"> Get Started</Link>
         </button>
         </div>
         
